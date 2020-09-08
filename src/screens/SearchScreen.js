69a1b5c7498b076e4fb4e7e13d0ba2915728b3b5
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-08 23:07:22 +0900

    #95 Managing State

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [Term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={Term} onTermChange={(newTerm) => setTerm(newTerm)} />
       <Text>Search Screen</Text>
+      <Text>{Term}</Text>
     </View>
   );
 };
